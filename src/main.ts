bcdc52c4efad10e6c54a505474f077f439750eb5
elturpin <laurent.turpin@mailo.com>
2024-08-28 21:38:24 +0200

    feat: allow to play the sound of an input file

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,28 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const audioContext = new AudioContext();
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const audioElement = document.querySelector("audio")!;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const track = audioContext.createMediaElementSource(audioElement);
+
+track.connect(audioContext.destination);
+
+const playButton = document.querySelector("button")!;
+const fileInput = document.querySelector("input")!;
+
+fileInput.addEventListener("input", () => {
+    if (fileInput.files?.[0]) {
+        audioElement.src = URL.createObjectURL(fileInput.files[0]);
+        playButton.disabled = false;
+    }
+})
+
+playButton.addEventListener(
+    "click",
+    () => {
+        // Check if context is in suspended state (autoplay policy)
+        if (audioContext.state === "suspended") {
+            audioContext.resume();
+        }
+        audioElement.play();
+    }
+);
